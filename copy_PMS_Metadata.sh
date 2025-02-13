b84435cadea2043555654c2afaf088b6c3e81ce7
Paul Orion Weaver <awesomeboy7865@gmail.com>
2025-02-13 16:08:54 -0700

    Update copy_PMS_Metadata.sh

--- copy_PMS_Metadata.sh
+++ copy_PMS_Metadata.sh
@@ -11,15 +11,14 @@ echo "docker stop plex: $(docker stop plex)" >> "$LOG_FILE"
 
 echo "Starting compression of $SOURCE to $DEST$ARCHIVE_NAME" >> "$LOG_FILE"
 
-# Create the tar.gz archive while preserving permissions/ownership
 tar -czpvf "$DEST$ARCHIVE_NAME" -C "$SOURCE" . >> "$LOG_FILE" 2>&1
 
 if [ $? -eq 0 ]; then
     echo "Compression completed successfully." >> "$LOG_FILE"
-    echo "Archive created at: $DEST$ARCHIVE_NAME" # useful when script is run manually
+    echo "Archive created at: $DEST$ARCHIVE_NAME" 
 else
     echo "Compression failed. Check the log file for details: $LOG_FILE" >> "$LOG_FILE"
-    echo "Compression failed." # useful when script is run manually
+    echo "Compression failed."
 fi
 
 echo "Starting up Plex container." >> "$LOG_FILE"
