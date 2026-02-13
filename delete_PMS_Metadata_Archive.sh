c3bc8bd47877e827b2d1e91c3fc9a8ec5e5eaf2b
Paul Orion Weaver <awesomeboy7865@gmail.com>
2026-02-12 23:24:12 -0700

    Delete delete_PMS_Metadata_Archive.sh

--- delete_PMS_Metadata_Archive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls -t /media/plex/Backup/BackupPlexServerContents/PlexMetadataParent/PlexMetadata/*.tar.gz | tail -n +4 | while read file; 
-
-do
-  echo "Deleting: $file"
-  rm -f "$file"
-done > /var/log/pms_backup/delete_metadata.txt 2>&1
